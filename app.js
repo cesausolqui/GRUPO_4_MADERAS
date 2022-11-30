97193dae78edc6e81dc48d31cf3e6f2aaf92464f
Moisés Tejada <moitejadaa@gmail.com>
2022-11-30 11:28:39 -0300

    .

--- app.js
+++ app.js
@@ -3,26 +3,31 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log('Aplicación corriendo en el puerto http://localhost:3000');
-});
+const mainRoutes = require('./routes/mainRoutes');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use('/', mainRoutes);
+
+const usersRoutes = require('./routes/usersRoutes');
+app.use('/users', usersRoutes);
+
 
 app.use(express.static(path.resolve(__dirname, 'public'))
 );
 
 //Routes o rutas
 
-app.get('/', (request, response) => {
-    response.sendFile(path.resolve(__dirname, './views/index.html'));
-});
+// app.get('/', (request, response) => {
+    // response.sendFile(path.resolve(__dirname, './views/index.html'));
+// });
 
 app.get('/register.html', (request, response) => {
     response.sendFile(path.resolve(__dirname, './views/register.html'));
 });
 
-app.get('/login.html', (request, response) => {
-    response.sendFile(path.resolve(__dirname, './views/login.html'));
-});
+//app.get('/login.ejs', (request, response) => {
+//    response.sendFile(path.resolve(__dirname, './views/login.html'));
+//});
 
 app.get('/productDetail.html', (request, response) => {
     response.sendFile(path.resolve(__dirname, './views/productDetail.html'));
@@ -30,4 +35,8 @@ app.get('/productDetail.html', (request, response) => {
 
 app.get('/productCart.html', (request, response) => {
     response.sendFile(path.resolve(__dirname, './views/productCard.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('Aplicación corriendo en el puerto http://localhost:3000');
 });
